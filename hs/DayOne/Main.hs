e1e5d146b86f45cd80030e5a982bc536a51341b5
Gareth Rogers <gareth.j.rogers@gmail.com>
2018-12-22 19:28:55 +0000

    Adding day 1.1 solution

--- hs/DayOne/Main.hs
+++ hs/DayOne/Main.hs
@@ -1,16 +1,39 @@
 module Main where
 
+import qualified Data.Set as Set
+
 main :: IO ()
 main = do
   putStrLn "Advent of Code Day 1.1"
   contents <- getContents
-  putStr "The adjusted frequency is: "
-  print $ adjustFrequency contents
+  print $ "The adjusted frequency is: " ++ show (adjustFrequency contents)
+  putStrLn "Advent of Code Day 1.2"
+  print $ "The repeated frequency is: " ++ show
+    (findRepeatedFrequency (0, Set.singleton 0) $ (cycle . lines) contents)
+
 
 read' :: String -> Int
 read' s
   | head s == '+' = read $ tail s
   | otherwise = read s
 
+addString :: String -> Int -> Int
+addString s i =  read' s + i
+
 adjustFrequency :: String -> Int
-adjustFrequency contents = foldl (\acc x -> acc + read' x) 0 (lines contents)
+adjustFrequency contents = foldr addString 0 (lines contents)
+
+addFrequency :: String -> Int -> Set.Set Int -> (Int, Set.Set Int)
+addFrequency adjustment frequency frequencies =
+  (frequency', Set.insert frequency' frequencies)
+  where frequency' = addString adjustment frequency
+
+checkFrequency :: Int -> Set.Set Int -> Bool
+checkFrequency frequency frequencies = Set.member frequency frequencies
+
+findRepeatedFrequency :: (Int, Set.Set Int) -> [String] -> Int
+findRepeatedFrequency _ [] = error "No frequencies given to check"
+findRepeatedFrequency (frequency, frequencies) (s:ss)
+  | checkFrequency frequency' frequencies = frequency'
+  | otherwise = findRepeatedFrequency (frequency', frequencies') ss
+  where (frequency', frequencies') = addFrequency s frequency frequencies
